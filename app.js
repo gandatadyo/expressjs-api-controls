965aefbf33e179421b19c9424527d4e0fc8ba370
gandatadyo <gandatadyosurya@gmail.com>
2024-04-12 18:31:21 +0700

    update

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.get('/', (req, res) => {
+  res.json({ success: true, message: 'is running' });
+});
+
 // Rute untuk mengembalikan hasil success
 app.get('/success', (req, res) => {
   res.json({ success: true, message: 'Request was successful' });
